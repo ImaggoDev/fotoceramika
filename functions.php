43e782230a204e5c4bb70519438c14109b9b7544
Grzegorz Rydzkowski <grzesiek.rydzkowski@gmail.com>
2022-04-22 16:30:09 +0200

    add sync for acf fields

--- functions.php
+++ functions.php
@@ -21,22 +21,22 @@ function porto_child_css()
         wp_enqueue_style('styles-child-rtl');
     }
 }
+
 // Load JS
-function register_scripts() {
-  //  wp_enqueue_script( 'custom-js', get_template_directory_uri() . '/assets/scripts/custom.js', array(), '1', true );
+function register_scripts()
+{
+    //  wp_enqueue_script( 'custom-js', get_template_directory_uri() . '/assets/scripts/custom.js', array(), '1', true );
 }
-add_action( 'wp_enqueue_scripts', 'register_scripts' );
+
+add_action('wp_enqueue_scripts', 'register_scripts');
 // Woo
 add_filter('wc_product_sku_enabled', '__return_false');
-
-
 
 add_action('woocommerce_after_single_product_summary', function () {
     echo '<div class="custom-product-designer" style="width: 100%; margin-bottom: 1rem;">';
     do_action('fpd_product_designer');
     echo '</div>';
 });
-
 
 /**
  *
@@ -46,25 +46,24 @@ add_action('woocommerce_after_single_product_summary', function () {
 
 add_filter('woocommerce_variable_sale_price_html', function ($price, $product) {
     // Sale Price
-        $prices = array( $product->get_variation_regular_price( 'min', true ), $product->get_variation_regular_price( 'max', true ) );
-        sort( $prices );
-        $saleprice = $prices[0] !== $prices[1] ? sprintf( __( 'od %1$s', 'woocommerce' ), wc_price( $prices[0] ) ) : wc_price( $prices[0] );
+    $prices = array($product->get_variation_regular_price('min', true), $product->get_variation_regular_price('max', true));
+    sort($prices);
+    $saleprice = $prices[0] !== $prices[1] ? sprintf(__('od %1$s', 'woocommerce'), wc_price($prices[0])) : wc_price($prices[0]);
 
-        if ( $price !== $saleprice ) {
+    if ($price !== $saleprice) {
         $price = '<del>' . $saleprice . $product->get_price_suffix() . '</del> <ins>' . $price . $product->get_price_suffix() . '</ins>';
-        }
-        return $price;
-},10, 2);
-
+    }
+    return $price;
+}, 10, 2);
 
 add_filter('woocommerce_variable_price_html', function ($price, $product) {
     // Main Price
-    if(!is_product()) {
-        $prices = array( $product->get_variation_price( 'min', true ), $product->get_variation_price( 'max', true ) );
-        $price = $prices[0] !== $prices[1] ? sprintf( __( 'od %1$s', 'woocommerce' ), wc_price( $prices[0] ) ) : wc_price( $prices[0] );
+    if (!is_product()) {
+        $prices = array($product->get_variation_price('min', true), $product->get_variation_price('max', true));
+        $price = $prices[0] !== $prices[1] ? sprintf(__('od %1$s', 'woocommerce'), wc_price($prices[0])) : wc_price($prices[0]);
     }
     return $price;
-},10, 2);
+}, 10, 2);
 
 /**
  *
@@ -72,13 +71,14 @@ add_filter('woocommerce_variable_price_html', function ($price, $product) {
  *
  */
 
-add_filter( 'woocommerce_catalog_orderby', 'rename_default_sorting_options' );
+add_filter('woocommerce_catalog_orderby', 'rename_default_sorting_options');
 
-function rename_default_sorting_options( $options ){
+function rename_default_sorting_options($options)
+{
 
-	$options[ 'menu_order' ] = 'Podstawowe sortowanie';
+    $options['menu_order'] = 'Podstawowe sortowanie';
 
-	return $options;
+    return $options;
 
 }
 
@@ -87,31 +87,49 @@ function rename_default_sorting_options( $options ){
  * Woocommerce product tab change name
  *
  */
-add_filter( 'woocommerce_product_tabs', 'rename_additional_info_tab' );
+add_filter('woocommerce_product_tabs', 'rename_additional_info_tab');
 
-function rename_additional_info_tab( $tabs ) {
+function rename_additional_info_tab($tabs)
+{
 
-	$tabs['additional_information']['title'] = 'Dodatkowe informacje';
+    $tabs['additional_information']['title'] = 'Dodatkowe informacje';
     $tabs['description']['title'] = 'Opis produktu';
-	return $tabs;
+    return $tabs;
 
 }
 
+// ACF SYnc
+add_filter('acf/json_directory', function ($path) {
+    return get_template_directory() . '/acf-json';
+});
 
-remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
-remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_rating', 10 );
+add_filter('acf/settings/save_json', function ($path) {
 
-add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 8 );
-add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_rating', 5 );
+    return apply_filters("acf/json_directory", NULL);
 
+});
+
+add_filter('acf/settings/load_json', function ($paths) {
+
+    return [
+        apply_filters("acf/json_directory", NULL)
+    ];
+});
+
+remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_title', 5);
+remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_rating', 10);
+
+add_action('woocommerce_single_product_summary', 'woocommerce_template_single_title', 8);
+add_action('woocommerce_single_product_summary', 'woocommerce_template_single_rating', 5);
 
 add_action('woocommerce_before_add_to_cart_form', 'custom_product_desc');
 
-function custom_product_desc() {
+function custom_product_desc()
+{
     global $post;
     $fpd = get_post_meta($post->ID, 'fpd_product_settings');
 
-    if(empty($fpd)) {
+    if (empty($fpd)) {
         return false;
     }
     ob_start();
