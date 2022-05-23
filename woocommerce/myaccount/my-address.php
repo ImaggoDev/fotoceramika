f4d3d480659d5439000bb851d92d180ed5364db3
Norbert Jańczak <leon@MacBook-Pro-Norbert.local>
2022-05-23 09:35:17 +0200

    fix address fields

--- woocommerce/myaccount/my-address.php
+++ woocommerce/myaccount/my-address.php
@@ -69,29 +69,8 @@ if ( ! wc_ship_to_billing_address_only() && $shipping_enabled ) {
 
 		<address>
 		<?php
-        $address = apply_filters( 'woocommerce_my_account_my_address_formatted_address', array(
-			'first_name'  => get_user_meta( $customer_id, $name . '_first_name', true ),
-			'last_name'   => get_user_meta( $customer_id, $name . '_last_name', true ),
-			'company'     => get_user_meta( $customer_id, $name . '_company', true ),
-			'address_1'   => get_user_meta( $customer_id, $name . '_address_1', true ),
-			'address_2'   => get_user_meta( $customer_id, $name . '_address_2', true ),
-			'city'        => get_user_meta( $customer_id, $name . '_city', true ),
-			'state'       => get_user_meta( $customer_id, $name . '_state', true ),
-			'postcode'    => get_user_meta( $customer_id, $name . '_postcode', true ),
-			'country'     => get_user_meta( $customer_id, $name . '_country', true )
-		), $customer_id, $name );
-	
-		$formatted_address = WC()->countries->get_formatted_address( $address );
-	
-		if ( ! $formatted_address )
-			_e( 'You have not set up this type of address yet.', 'woocommerce' );
-		else
-			$pattern = "/<br\/>/";
-			$replacements= '<p>';
-			$formatted_address = WC()->countries->get_formatted_address( $address );
-			$output = preg_replace($pattern, $replacements, $formatted_address);
-			echo "<p>" . $output . "</p>";
-    ?>
+			echo ! $address ? esc_html_e( 'You have not set up this type of address yet.', 'woocommerce' ) : wp_kses_post( $address );
+		?>
 		</address>
 		<a href="<?php echo esc_url( wc_get_endpoint_url( 'edit-address', $name ) ); ?>" class="edit button wc-action-btn mt-3"><?php echo ! $address ? esc_html__( 'Edytuj dane', 'porto' ) : esc_html__( 'Edytuj dane', 'porto' ); ?><i class="icon icon_btn_arrow"></i></a>
 	</div>
