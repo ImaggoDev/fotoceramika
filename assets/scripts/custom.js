3ee086d2b756b9234df2158c60a5232d0bff629f
Norbert Jańczak <leon@MacBook-Pro-Norbert.local>
2022-06-28 09:13:56 +0200

    Add required variations

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -1,105 +1,110 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const SidebarLimitElement = () => {};
+  SidebarLimitElement();
 
-
-const SidebarLimitElement = () => {
-
-}
-SidebarLimitElement();
-
-
-setTimeout( () => {
+  setTimeout(() => {
     setCanvasBgStyle();
     scrollToPersonalize();
     custom_fpd_attr();
-}, 800);
-
-
+  }, 800);
 });
 
-
-
 function setCanvasBgStyle() {
-    let bg_element = document.querySelector('.fpd-main-wrapper');
+  let bg_element = document.querySelector(".fpd-main-wrapper");
 
-    if(!bg_element) {
-        return false;
-    }
-    bg_element.classList.add('is-custom');
+  if (!bg_element) {
+    return false;
+  }
+  bg_element.classList.add("is-custom");
 }
 
 function scrollToPersonalize() {
-    let btn = document.querySelector('.button-personalize-fpd ');
+  let btn = document.querySelector(".button-personalize-fpd ");
 
-    if(!btn) {
-        return false;
-    }
+  if (!btn) {
+    return false;
+  }
 
-    btn.addEventListener('click',  () => {
-        let target = document.querySelector('.custom-product-designer');
-        const margin = window.innerWidth * 0.1 < 80 ? window.innerWidth * 0.1 : 80;
-        var rect = target.getBoundingClientRect();
+  btn.addEventListener("click", () => {
+    let target = document.querySelector(".custom-product-designer");
+    const margin = window.innerWidth * 0.1 < 80 ? window.innerWidth * 0.1 : 80;
+    var rect = target.getBoundingClientRect();
 
-        scroll({
-            top: rect.top - margin,
-            behavior: "smooth"
-          });
-    })
-
+    scroll({
+      top: rect.top - margin,
+      behavior: "smooth",
+    });
+  });
 }
 
 function custom_fpd_attr() {
-    let wrapper = document.querySelector('.fpd-product-checkout-form');
-    if(!wrapper) {
-        return false;
-    }
+  let wrapper = document.querySelector(".fpd-product-checkout-form");
+  if (!wrapper) {
+    return false;
+  }
 
-    let buttons = wrapper.querySelectorAll('button');
-    let select_variant = wrapper.querySelector('select[name="variant"]');
-    let variants_wrappers = wrapper.querySelectorAll('[data-variant]');
+  let buttons = wrapper.querySelectorAll("button");
+  let select_variant = wrapper.querySelector('select[name="variant"]');
+  let variants_wrappers = wrapper.querySelectorAll("[data-variant]");
 
-    console.log(variants_wrappers);
+  console.log(variants_wrappers);
 
-    buttons.forEach((e)=>{
-        e.addEventListener('click', (evt) => {
-            evt.preventDefault();
-            for(let item in e.dataset) {
-                let select = document.querySelector('select[name="'+item+'"]');
-                if(select) {
-                    select.value = e.dataset[item];
-                    select.dispatchEvent(new Event('change',{"bubbles": true}));
-                    // jQuery option: $('select[name="'+item+'"]').val( e.dataset[item] ).change();
-                }
-            }
-
-            buttons.forEach((btn)=>{
-                btn.classList.remove('is-active');
-            })
-            e.classList.add('is-active');
-        })
-    })
-
-
-    select_variant.addEventListener('change', (e) => {
-        let value = e.target.value;
-        let is_active = false;
-        let woo_select = document.querySelector('select[name="attribute_rozmiar"]');
-        console.log(woo_select);
-
-        variants_wrappers.forEach( (item) => {
-            const element_data = item.dataset.variant;
-            if(element_data == value && !is_active) {
-                item.classList.add('active');
-                is_active = true;
-            } else {
-                item.classList.remove('active');
-            }
-        })
-
-        if(woo_select) {
-            woo_select.value = value;
-            console.log(woo_select.value);
-            woo_select.dispatchEvent(new Event('change',{"bubbles": true}));
+  buttons.forEach((e) => {
+    e.addEventListener("click", (evt) => {
+      evt.preventDefault();
+      for (let item in e.dataset) {
+        let select = document.querySelector('select[name="' + item + '"]');
+        if (select) {
+          select.value = e.dataset[item];
+          select.dispatchEvent(new Event("change", { bubbles: true }));
+          // jQuery option: $('select[name="'+item+'"]').val( e.dataset[item] ).change();
         }
-    })
+      }
 
+      buttons.forEach((btn) => {
+        btn.classList.remove("is-active");
+      });
+      e.classList.add("is-active");
+    });
+  });
+
+  select_variant.addEventListener("change", (e) => {
+    let value = e.target.value;
+    let is_active = false;
+    let woo_select = document.querySelector('select[name="attribute_rozmiar"]');
+    console.log(woo_select);
+
+    variants_wrappers.forEach((item) => {
+      const element_data = item.dataset.variant;
+      if (element_data == value && !is_active) {
+        item.classList.add("active");
+        is_active = true;
+      } else {
+        item.classList.remove("active");
+      }
+    });
+
+    if (woo_select) {
+      woo_select.value = value;
+      console.log(woo_select.value);
+      woo_select.dispatchEvent(new Event("change", { bubbles: true }));
+    }
+  });
+
+  function requiredVariations() {
+    const variant = document.querySelector('select[name="variant"]');
+    const addToCart = document.querySelector(".single_add_to_cart_button");
+
+    if (variant) {
+      variant.required = true;
+      addToCart.classList.add("wc-variation-selection-needed");
+
+      addToCart.addEventListener("click", (e) => {
+        if (!variant.value) {
+          alert("Wybierz opcje produktu przed dodaniem go do koszyka.");
+        }
+      });
+    }
+  }
+  requiredVariations();
 }
